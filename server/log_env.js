37bd65d75335628e5e8466c08cafa483df65d185
kismetkrystle <kismet.krystle@gmail.com>
2025-12-17 12:17:38 +0800

    Cleanup debug scripts

--- server/log_env.js
+++
@@ -1,7 +0,0 @@
-
-require('dotenv').config();
-const fs = require('fs');
-
-const dbUrl = process.env.DATABASE_URL;
-fs.writeFileSync('debug_env.txt', `URL: [${dbUrl}]\nLength: ${dbUrl ? dbUrl.length : 0}`);
-console.log('Done writing debug_env.txt');
